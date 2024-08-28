5657e67d4df4f1ace3496fca5531552de4a2bce7
Sujan khatri <sujan.chettri1321@gmail.com>
2024-08-28 14:18:32 +0545

    heyman

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,36 @@
-
+import { Link } from "react-router-dom";
+import { FOOTER_LINKS } from "../assets/data";
+import { Children } from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div>
+      <div>
+        <Link to="/" className="mb-10 bold -20">
+          Shoppee
+        </Link>
+        <div>
+          {FOOTER_LINKS.map((col) => (
+            <FooterColumn title={col.title}  key={col.title} >
+              <ul>
+                {col.links.map((link))}
+              </ul>
+            </FooterColumn>
+          ))}
+        </div>
+      </div>
+      </div>
+    </footer>
+  );
+};
+
+const FooterColumn=({title,Children})=>{
+  return(
+    <div>
+      <h4>{title}</h4>
+      {Children}
+    </div>
+  );
 }
 export default Footer;
