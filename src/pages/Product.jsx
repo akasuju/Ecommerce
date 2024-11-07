0455de4b97b5fcf02a6eead355c8bdbf9af4e2b3
Sujan khatri <sujan.chettri1321@gmail.com>
2024-11-07 15:21:46 +0545

    error

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,20 +1,19 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { ShopContext } from "../context/ShopContext";
+import { ShopContext } from "../contexts/ShopContext";
 import ProductHD from "../component/ProductHD";
 
- const Product = () => {
-  const {all_products} =useContext(ShopContext);
-  const {productId}= useParams();
-  const product =all_products.find((e)=>e.id === Number(productId))
+const Product = () => {
+  const { all_products } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_products.find((e) => e.id === Number(productId));
   return (
-<section>
-  <div>
-    <ProductHD product={product}/>
-  </div>
-</section>
-  )
-    
-  
-}
-export default Product
+    <section>
+      <div>
+        <ProductHD product={product} />
+      </div>
+    </section>
+  );
+};
+
+export default Product;
