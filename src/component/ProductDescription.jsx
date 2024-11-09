7144c55ab375967db6a348280658960f9ac67fc8
Sujan khatri <sujan.chettri1321@gmail.com>
2024-11-09 22:04:17 +0545

    hello there

---
+++ src/component/ProductDescription.jsx
@@ -0,0 +1,33 @@
+const ProductDescription = () => {
+  return (
+    <div className="mt-20">
+      <div className="flex gap-3 mb-4 ">
+        <button className="btn_dark_rounded !rounded-none !text-xs !py-[6px] w-36">
+          Description
+        </button>
+        <button className="btn_dark_outline !rounded-none !text-xs !py-[6px] w-36">
+          Care Guide
+        </button>
+        <button className="btn_dark_outline !rounded-none !text-xs !py-[6px] w-36">
+          Size Guide
+        </button>
+      </div>
+      <div className="flex flex-col pb-16">
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero saepe
+          fugiat mollitia in, laborum magnam illum ipsa, molestias tempora harum
+          et corrupti eligendi labore dolore deleniti ipsam soluta at?
+          Reiciendis earum molestiae eaque et dicta blanditiis magni? Tempore id
+          veritatis sit eaque animi. Impedit distinctio ratione magnam saepe
+          tenetur iusto. Autem vitae unde itaque sequi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          fugiat facilis id vel ullam debitis harum, at molestiae eius atque.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDescription;
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,10 @@
+import CartItems from "../component/CartItems";
+
+const Cart = () => {
+  return (
+    <div>
+      <CartItems />
+    </div>
+  )
+}
+export default Cart;
