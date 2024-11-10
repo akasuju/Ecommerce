41e2ad5ae6b3c482f8d32dbe954135703d240509
Sujan khatri <sujan.chettri1321@gmail.com>
2024-11-10 21:48:20 +0545

    feat: initialize React app with Vite and Tailwind CSS setup

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const path = require("path");
 const cors = require("cors");
+const { Console } = require("console");
 
 app.use(cors());
 app.use(express.json());
@@ -18,98 +19,111 @@ mongoose.connect(
 //API creation
 
 app.get("/", (req, res) => {
-    res.send("xpress is running");
+  res.send("xpress is running");
 });
 
 //Image storage engine
 const storage = multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-    }
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 //creating upload endpoint for images
-app.use("/upload",express.static("upload/images"));
+app.use("/upload", express.static("upload/images"));
 
-app.post("/upload",upload.single('product'),(req,res)=>{
-    res.json({
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
     sucess: 1,
-    Image_url: `http://localhost:${port}/images/${req.file.filename}`    
-    });
+    Image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
 });
 //schema for creating products
-const Product=mongoose.model("product",{
-    id:{
-      type: Number,
-        required: true,
-    },
-    name:{
-        type: String,
-        required: true,
-    },
-    //image:{
-        //type: String,
-        //required: true,
-    //},
-    category:{
-        type: String,
-        required: true,
-    },
-    new_price:{
-        type: Number,
-        required: false,
-    },
-    old_price:{
-        type: Number,
-        required: true,
-    },
-    Date:{
-        type: Date,
-        default: Date.now,
-    },
-    available:{
-        type: Boolean,
-        required: true,
-    },
-
+const Product = mongoose.model("product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    required: true,
+  },
 });
 
-
-app.post("/addproduct",async(req,res)=>{
-    let products = await Product.find({});
-let id;
-if(products.length>0){
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
     let last_product_array = products.slice(-1);
-let last_product = last_product_array[0];
-id = last_product.id+1;
-}else{
-    id=1;
-}
-   const product=new Product({
-    id:id,
-    name:req.body.name,
-    image:req.body.image,
-    category:req.body.category,
-   })
-console.log(product);
-   await product.save();
-   console.log("Product added");
-   res.json({
-sucess:true,
-message:"Product added",
-
-   });
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Product added");
+  res.json({
+    sucess: true,
+    message: "Product added",
+  });
 });
 
 // Creating api for add Product
-app.post('removeproduct',async(req,res)=>{
-    const id = req.body.id;})
+app.post("removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Product removed");
+  res.json({
+    sucess: true,
+    name: req.body.name,
+  });
+});
 
-app.listen(port,(error)=>{
-    if(error){
-        console.log("Error in running server"+ error);
-    }
-    console.log("Server is running on port:"+port);
-} )
+//Creating api for get all products
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  Console.log("All Products are fetched");
+  res.send(products);
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("Error in running server" + error);
+  }
+  console.log("Server is running on port:" + port);
+});
