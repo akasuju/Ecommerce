a794df992b69d699e18517657fd7c148e3ae5640
Sujan khatri <sujan.chettri1321@gmail.com>
2024-11-14 22:07:05 +0545

    feat: implement signup functionality and toggle between login and signup states in Login component

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,22 +1,74 @@
+import { useState } from "react";
 
 const Login = () => {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+const login =async ()=>{
+  console.log("login")
+
+
+}
+const signup =async ()=>{
+console.log("signup");
+}
+
   return (
     <section className="max_padd_container flexCenter flex-col pt-32">
       <div>
         <h3 className="h3">Sign Up</h3>
         <div className="flex flex-col gap-4 mt-7">
-          <input type="text" placeholder="Your Name" className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl " />
-          <input type="email" placeholder="Email Address" className="h-14 w-full pl-5  bg-slate-900/5 outline-none rounded-xl"  />
-          <input type="Password" placeholder="Password" className="h-14 w-full pl-5  bg-slate-900/5 outline-none rounded-xl" />
+          {state === "Sign Up" ? (
+            <input
+              name="username"
+              type="text"
+              placeholder="Your Name"
+              className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl "
+            />
+          ) : (
+            ""
+          )}
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="h-14 w-full pl-5  bg-slate-900/5 outline-none rounded-xl"
+          />
+          <input
+            type="Password"
+            placeholder="Password"
+            className="h-14 w-full pl-5  bg-slate-900/5 outline-none rounded-xl"
+          />
         </div>
-        <button className="btn_dark_rounded my-5 w-full !rounded-md">Continue</button>
-        <p className="text-black font-bold">Already have an Account? <span className="text-secondary underline cursor-pointer">Login</span></p>
-        <div className="flexcenter mt-6 gap-3">
-          <input type="checkbox" name="" id=""/>
+        <button onClick={()=>{state==="Login"?login():signup()}} className="btn_dark_rounded my-5 w-full !rounded-md">
+          Continue
+        </button>
+
+        {state==="Sign Up"?<p className="text-black font-bold">
+          Already have an Account?
+          <span onClick={()=>{setState("Login") }} className="text-secondary underline cursor-pointer">Login</span>
+        </p> 
+:
+        <p className="text-black font-bold">
+          Create an account?
+          <span onClick={()=>{setState("Sign Up") }} className="text-secondary underline cursor-pointer">
+            Click here
+          </span>
+        </p>}
+
+        <div className="flexCenter mt-6 gap-3">
+          <input type="checkbox" name="" id="" />
           <p>By continuing, I agree to terms and condition.</p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 export default Login;
